9d0b6346170316cf2e46b08b9a70eeaf7585f43e
Abril <francoasofia@gmail.com>
2022-10-30 18:03:08 -0300

    Creación y parte del back

---
+++ ProyectoIntegrador_FA/Backend/src/main/java/com/portfolio/af/AfApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.af;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_FA/Backend/src/main/java/com/portfolio/af/Interface/IPersonaService.java
@@ -0,0 +1,24 @@
+package com.portfolio.af.Interface;
+
+import com.portfolio.af.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona > getPersona();
+    
+
+
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+    
+
+
+}
